732487eefd4dace9b1c08c4493f986afc2bf33ea
Igor Assis Baroni <73115394+IgorBaroni@users.noreply.github.com>
2022-09-20 14:04:46 -0300

    add routing.module com residente path

--- tcc_pv_front/src/app/app-routing.module.ts
+++ tcc_pv_front/src/app/app-routing.module.ts
@@ -3,14 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { CadastroComponent } from './Views/cadastro/cadastro.component';
 import { EstoqueComponent } from './Views/estoque/estoque.component';
 import { FinanceiroComponent } from './Views/financeiro/financeiro.component';
-import { HomeComponent } from './Views/home/home.component';
+import { ResidenteComponent } from './Views/residente/residente.component';
 import { ModaltesteComponent } from './Views/modalteste/modalteste.component';
+import { HomeComponent } from './Views/home/home.component';
 
 const routes: Routes = [{path:'',component: HomeComponent,},
 {path:'cadastro',component: CadastroComponent,},
 {path:'financeiro',component: FinanceiroComponent,},
 {path:'estoque',component: EstoqueComponent,},
-{path:'modalteste',component: ModaltesteComponent,},]
+{path:'modalteste',component: ModaltesteComponent,},
+{path:'residente',component: ResidenteComponent,},]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
