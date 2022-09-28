e6fe742bab362c091f31a64d027fc84330ce1d28
Eduardo Serati <eduserati@gmail.com>
2022-09-28 16:17:20 -0300

    Rotas_Tabelas

--- tcc_pv_front/src/app/app-routing.module.ts
+++ tcc_pv_front/src/app/app-routing.module.ts
@@ -6,21 +6,37 @@ import { FinanceiroComponent } from './Views/financeiro/financeiro.component';
 import { ResidenteComponent } from './Views/residente/residente.component';
 import { HomeComponent } from './Views/home/home.component';
 import { MovimentacaoComponent } from './Views/movimentacao/movimentacao.component';
+import { Tabela_caixaComponent } from './Views/tabela_caixa/tabela_caixa.component';
+import { Tabela_verbaestadualComponent } from './Views/tabela_verbaestadual/tabela_verbaestadual.component';
+import { Tabela_verbamunicipalComponent } from './Views/tabela_verbamunicipal/tabela_verbamunicipal.component';
+import { Tabela_emendaparlamentarComponent } from './Views/tabela_emendaparlamentar/tabela_emendaparlamentar.component';
+import { Tabela_emendaimpositivaComponent } from './Views/tabela_emendaimpositiva/tabela_emendaimpositiva.component';
+import { Tabela_contacorrenteComponent } from './Views/tabela_contacorrente/tabela_contacorrente.component';
+import { Tabela_contacapitalComponent } from './Views/tabela_contacapital/tabela_contacapital.component';
 import { ConsultacadastroComponent } from './Views/consultacadastro/consultacadastro.component';
 import { ConsultafinanceiroComponent } from './Views/consultafinanceiro/consultafinanceiro.component';
 import { ConsultaestoqueComponent } from './Views/consultaestoque/consultaestoque.component';
-import { Tabela_caixaComponent } from './Views/financeiro/tabelas/tabela_caixa/tabela_caixa.component';
 
-const routes: Routes = [{path:'',component: HomeComponent,},
-{path:'cadastro',component: CadastroComponent,},
-{path:'financeiro',component: FinanceiroComponent,},
-{path:'estoque',component: EstoqueComponent,},
-{path:'residente',component: ResidenteComponent,},
-{path:'movimentacao',component: MovimentacaoComponent},
-{path:'consultacadastro',component: ConsultacadastroComponent},
-{path:'consultafinanceiro',component:ConsultafinanceiroComponent},
-{path:'consultaestoque',component: ConsultaestoqueComponent},
-{path:'tabela_caixa',component: Tabela_caixaComponent},]
+
+const routes: Routes = [
+  {path:'', component: HomeComponent,},
+  {path:'cadastro', component: CadastroComponent,},
+  {path:'financeiro', component: FinanceiroComponent,},
+  {path:'estoque' ,component: EstoqueComponent,},
+  {path:'residente', component: ResidenteComponent,},
+  {path:'movimentacao', component: MovimentacaoComponent},
+  {path:'consultacadastro', component: ConsultacadastroComponent},
+  {path:'consultafinanceiro', component:ConsultafinanceiroComponent},
+  {path:'consultaestoque', component: ConsultaestoqueComponent},
+  {path:'tabela_caixa', component: Tabela_caixaComponent},
+  {path:'tabela_contacapital', component: Tabela_contacapitalComponent},
+  {path:'tabela_contacorrente', component: Tabela_contacorrenteComponent},
+  {path:'tabela_emendaimpositiva', component: Tabela_emendaimpositivaComponent},
+  {path:'tabela_emendaparlamentar', component: Tabela_emendaparlamentarComponent},
+  {path:'tabela_verbaestadual', component: Tabela_verbaestadualComponent},
+  {path:'tabela_verbamunicipal', component: Tabela_verbamunicipalComponent},
+
+]
 
 
 @NgModule({
--- tcc_pv_front/src/app/app.module.ts
+++ tcc_pv_front/src/app/app.module.ts
@@ -1,17 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './Views/home/home.component';
 import { CadastroComponent } from './Views/cadastro/cadastro.component';
 import { EstoqueComponent } from './Views/estoque/estoque.component';
 import { FinanceiroComponent } from './Views/financeiro/financeiro.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { MovimentacaoComponent } from './Views/movimentacao/movimentacao.component';
 import { ConsultacadastroComponent } from './Views/consultacadastro/consultacadastro.component';
 import { ConsultaestoqueComponent } from './Views/consultaestoque/consultaestoque.component';
 import { ConsultafinanceiroComponent } from './Views/consultafinanceiro/consultafinanceiro.component';
+import { Tabela_verbamunicipalComponent } from './Views/tabela_verbamunicipal/tabela_verbamunicipal.component';
+import { Tabela_verbaestadualComponent } from './Views/tabela_verbaestadual/tabela_verbaestadual.component';
+import { Tabela_emendaparlamentarComponent } from './Views/tabela_emendaparlamentar/tabela_emendaparlamentar.component';
+import { Tabela_emendaimpositivaComponent } from './Views/tabela_emendaimpositiva/tabela_emendaimpositiva.component';
+import { Tabela_contacorrenteComponent } from './Views/tabela_contacorrente/tabela_contacorrente.component';
+import { Tabela_contacapitalComponent } from './Views/tabela_contacapital/tabela_contacapital.component';
+import { Tabela_caixaComponent } from './Views/tabela_caixa/tabela_caixa.component';
 
 @NgModule({
   declarations: [
@@ -24,6 +30,15 @@ import { ConsultafinanceiroComponent } from './Views/consultafinanceiro/consulta
     ConsultacadastroComponent,
     ConsultaestoqueComponent,
     ConsultafinanceiroComponent,
+    Tabela_caixaComponent,
+    Tabela_contacapitalComponent,
+    Tabela_contacorrenteComponent,
+    Tabela_emendaimpositivaComponent,
+    Tabela_emendaparlamentarComponent,
+    Tabela_verbaestadualComponent,
+    Tabela_verbamunicipalComponent,
+
+
   ],
   imports: [
     BrowserModule,
